3208bc6d7a2b99b356eb09d724da6b4e77086edb
zhangyingwei <zhangyw_001@126.com>
2017-11-24 13:27:47 +0800

    完善了一下脚本

--- bin/treehole.sh
+++ bin/treehole.sh
@@ -1,99 +1,116 @@
 #!/bin/bash
 
-TREEHOLE_HOME="/Users/zhangyw/IdeaProjects/zhangyw/Projects/java/treehole-jekyll/treehole-jekyll"
+TREEHOLE_HOME="/home/zhangyingwei/java/treehole-jekyll-0.0.1-SNAPSHOT"
+LOG_NAME="treehole.out"
+
 env_args="-Xms128m -Xmx128m"
-sleeptime=0
 arglen=$#
 
-# get treehole pid
-get_pid(){
-    pname="`find .. -name 'treehole*.jar'`"
-    pname=${pname:3}
+function to_home(){
+    cd $TREEHOLE_HOME
+}
+
+function get_home(){
+    echo $TREEHOLE_HOME
+}
+
+function get_log_name(){
+    echo $LOG_NAME
+}
+
+function get_log_home(){
+    echo $(get_home)
+}
+
+function to_log_home(){
+    cd $(get_log_home)
+}
+
+function get_jar_name(){
+    to_home
+    jarname=`find *.jar`
+    echo $jarname
+}
+
+function get_pid(){
+    to_home
+    pname=$(get_jar_name)
     pid=`ps -ef | grep $pname | grep -v grep | awk '{print $2}'`
-    echo "$pid"
+    echo $pid
 }
 
-# get path
-get_path(){
-    path = $0
-    if [$path == "./treehole.sh"]
-    then
-        echo "aaaaaa"
-    else
-        echo "bbbb"
-    fi
-
-}
-
-show_status(){
+function startup(){
+    to_home
     pid=$(get_pid)
-    if [ "$pid" != "" ]
-    then
-        echo "TreeHole is running with pid: $pid"
+    if [ -n "$pid" ]; then
+        echo "Treehole is already running..."
     else
-        echo "TreeHole is stop!"
-    fi
-}
-
-startup(){
-    pid=$(get_pid)
-    if [ "$pid" != "" ]
-    then
-        echo "TreeHole already startup!"
-    else
-        jar_path=`find .. -name 'treehole*.jar'`
-        echo "jarfile=$jar_path"
-        cmd="java $1 -jar $jar_path > ../treehole.out < /dev/null &"
-        echo "cmd: $cmd"
-        java $1 -jar $jar_path > ./treehole.out < /dev/null &
+        jar_path=$(get_home)/$(get_jar_name)
+        echo $jar_path
+        cmd="java $env_args -jar $jar_path > $(get_log_home)/$(get_log_name) < /dev/null &"
+        echo $cmd
+        java $env_args -jar $jar_path > $(get_log_home)/$(get_log_name) < /dev/null &
         show_log
     fi
 }
 
-shut_down(){
+function show_log(){
+    to_log_home
+    tail -f $(get_log_name)
+}
+
+function shut_down(){
+    to_home
     pid=$(get_pid)
-    if [ "$pid" != "" ]
-    then
+    if [ -n "$pid" ]; then
         kill -9 $pid
-        echo "TreeHole is stop!"
+        echo "kill pid $pid ,Treehole is stop!"
     else
-        echo "TreeHole already stop!"
+        echo "Treehole is not running"
     fi
 }
 
-show_log(){
-    tail -f ../treehole.out
+function show_status(){
+    pid=$(get_pid)
+    if [ -n "$pid" ]; then
+        echo "Treehole is running with pid $pid"
+    else
+        echo "Treehole is stop!"
+    fi
 }
 
-show_help(){
+
+function show_help(){
     echo -e "\r\n\t欢迎使用TreeHole Blog"
     echo -e "\r\nUsage: sh treehole.sh start|stop|reload|status|log"
     exit
 }
 
-if [ $arglen -eq 0 ]
- then
+function reload_th(){
+    echo "reload..."
+    shut_down
+    startup
+}
+
+# start
+if [ $arglen -eq 0  ]; then
     show_help
 else
-    if [ "$2" != "" ]
-    then
-        env_args="$2"
-    fi
     case "$1" in
         "start")
-            startup "$env_args"
+            startup
             ;;
         "stop")
             shut_down
             ;;
         "reload")
-            echo "reload"
+            reload_th
             ;;
         "status")
             show_status
             ;;
         "log")
             show_log
-            ;;
+        ;;
     esac
 fi
